472c7d6923d593bc2d0820f6656f497b74e344b4
yunus emre alçık <yunus0emr2@hotmail.com>
2020-09-01 23:12:46 +0300

    uloo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ModalModule } from "ngx-bootstrap/modal";
+import { CustomFormsModule } from "ng2-validation";
+import { MobxAngularModule } from "mobx-angular";
+import { ContactsService } from "./contacts.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CustomFormsModule,
+    ModalModule.forRoot(),
+    MobxAngularModule
   ],
-  providers: [],
+  providers: [
+    ContactsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { ContactsService } from '../contacts.service';
+import { contactStore } from "../contact-store";
+import { identifierModuleUrl } from '@angular/compiler';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  modalRef: BsModalref;
+  store = contactStore;
+  edit: boolean;
+  contacts: any[] = [];
+  selectedContact: any = <any>{};
+  constructor() { 
+    private modalService: BsModalService,
+    private contactsService: ContactsService
+  } { }
+
+  openModal(addTemplate: TemplateRed<any>){
+    this.modalRef = this.modalService.show(addTemplate);
+  }
+
+  openEditModal(editTemplate: TemplateRef<any>, contact) {
+    this.selectedContact= contact;
+    this.modalRef = this.modalService.show(editTemplate);
+  }
+
+  closeModal() {
+    this.modalRed.hide();
+    this.selectedContact = {};
+  }
+
+  ngOnInit() {
+    this.contactsService.getContacts()
+    .subscribe(res => {
+      this.StorageEvent.setContacts(res);
+    })
+  }
+
+  deleteContact(id) {
+    this.contactsService.deleteContact(id)
+    .subscribe(res => {
+      this.getContacts();
+    })
+  }
+
+  getContacts() {
+    this.contactsService.getContacts()
+    .subscribe(res => {
+      this.StorageEvent.setContacts(res);
+    })
+  }
+}
